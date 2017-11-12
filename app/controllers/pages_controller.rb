9b385b89f0468fcbd5cecb14a2f74e9e8a7e3a4d
aurelien galoppe <agaloppe@me.com>
2017-11-12 19:24:23 +0100

    done

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,6 +1,9 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: [:home]
+  skip_before_action :authenticate_user!, only: [:home, :mobile]
 
   def home
+  end
+
+  def mobile
   end
 end
